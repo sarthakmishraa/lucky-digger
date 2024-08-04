900cbb204903c580cf5979616219dbb24ed11002
Sarthak Mishra <msarthak785@gmail.com>
2024-08-04 02:25:21 -0400

    minor change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,8 @@ export const Home = () => {
             <h1>Lucky Digger: Free Online Casino Games</h1>
             <h2>Play <Link to="/mines"><span>Mines</span></Link> & <Link to="/dice"><span>Dice</span></Link> now</h2>
             <div className="images-container">
-                <img src="../../public/mines_snip.png" className="images" />
-                <img src="../../public/dice_snip.png" className="images" />
+                <img src="./public/mines_snip.png" className="images" />
+                <img src="./public/dice_snip.png" className="images" />
             </div>
         </div>
     )
